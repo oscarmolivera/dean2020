9d60eab3cca38e5d4b2e2ba60f73dcdfa2b58924
Oscar M Olivera <oscarmolivera@gmail.com>
2020-05-13 00:25:05 -0400

    inicio del primer video

--- a_simpleblog/config/webpack/environment.js
+++ a_simpleblog/config/webpack/environment.js
@@ -4,6 +4,7 @@ const { environment } = require('@rails/webpacker')
   environment.plugins.append('Provide', new webpack.ProvidePlugin({
     $: 'jquery',
     jQuery: 'jquery',
+    Popper: ["popper.js", "default"],
     Rails: '@rails/ujs'
   }))
 
